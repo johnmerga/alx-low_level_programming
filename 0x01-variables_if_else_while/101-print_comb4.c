e4c50faab41d79443f2b6f09ddcdeed6ee999597
johnmerga <johnmerga@yahoo.com>
2021-06-18 21:43:44 +0300

    possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,32 +6,32 @@
  */
 int main(void)
 {
-    int i;
-    int j;
-    int k;
-    /* */
-    for (i = 0; i < 9; i++)
-    {
-        for (j = 1; j < 10; j++)
-        {
-            for (k = 1; k < 10; k++)
-            {
-                if (i != j && i != k && j != k && i < j && j < k)
-                {
-                    putchar(i + '0');
-                    putchar(j + '0');
-                    putchar(k + '0');
+	int i;
+	int j;
+	int k;
+	/* */
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			for (k = 1; k < 10; k++)
+			{
+				if (i != j && i != k && j != k && i < j && j < k)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
 
-                    if (i == 7 && j == 8 && k == 9)
-                    {
-                        break;
-                    }
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-    printf("\n");
-    return (0);
+					if (i == 7 && j == 8 && k == 9)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	printf("\n");
+	return (0);
 }
