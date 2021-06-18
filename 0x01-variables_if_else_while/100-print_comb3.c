2eaa3f7ec5dd4b791c0b33d11326159a1d9dc8e4
johnmerga <johnmerga@yahoo.com>
2021-06-18 20:58:38 +0300

    combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,19 +11,16 @@ int main(void)
 	/* */
 	for (i = 0; i < 9; i++)
 	{
-
 		for (j = 1; j < 10; j++)
 		{
 			if (i != j && i < j)
 			{
-
 				putchar(i + '0');
 				putchar(j + '0');
 				if (i == 8 && j == 9)
 				{
 					break;
 				}
-
 				putchar(',');
 				putchar(' ');
 			}
