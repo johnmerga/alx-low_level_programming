33f2350e14529bf2fbd0509c8c7a4ac359dc5b78
johnmerga <johnmerga@yahoo.com>
2021-06-22 19:44:39 +0300

    print Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *salut = "Holberton";
+	while (*salut)
+	{
+		putchar(*salut);
+		salut++;
+		}
+	putchar('\n');
+	return (0);
+}
