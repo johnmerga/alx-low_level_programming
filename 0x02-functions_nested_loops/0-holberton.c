05c2a75fd5e1ea50c06f2e4086251923eabb7112
johnmerga <johnmerga@yahoo.com>
2021-06-22 21:28:31 +0300

    print Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,17 +1,20 @@
 #include "holberton.h"
+
 /**
- * main - Entry point
+ * main - Prints Holberton as a message.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char *salut = "Holberton";
-	while (*salut)
+	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
 	{
-		putchar(*salut);
-		salut++;
-		}
-	putchar('\n');
+		_putchar(str[count]);
+	}
+	_putchar('\n');
 	return (0);
 }
