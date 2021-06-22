233ae34a48e3d032d1824bad820642f31d827b47
johnmerga <johnmerga@yahoo.com>
2021-06-22 21:37:55 +0300

    header

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,13 +7,11 @@
  */
 int main(void)
 {
-	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
-	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
+	char *salut = "Holberton";
+	while (*salut)
 	{
-		_putchar(str[count]);
+		_putchar(*salut);
+		salut++;
 	}
 	_putchar('\n');
 	return (0);
