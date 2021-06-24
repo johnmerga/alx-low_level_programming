12381d88d8604cb3d22f2fcfa6e981b5959934a3
johnmerga <johnmerga@yahoo.com>
2021-06-24 17:56:53 +0300

    prints the numbers, from 0 to 9 except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
 
 /**
- * print_most_numbers - prints numbers between 0 to 9
- * (neither 2 nor 4).
- * Return: no return.
- */
+ * print_most_numbers -  prints the numbers, from 0 to 9 except 2 and 4.
+*/
 void print_most_numbers(void)
 {
-	int ch;
+	int i;
 
-	for (ch = 48; ch < 58; ch++)
+	for (i = 48; i < 58; i++)
 	{
-		if (ch != 50 && ch != 52)
-			_putchar(ch);
+		if (i != 50 || i != 52)
+		{
+			_putchar(i);
+		}
 	}
 	_putchar('\n');
 }
