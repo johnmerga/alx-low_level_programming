fc1783e39fbb7a156ad648f8aeba4ced17709404
johnmerga <johnmerga@yahoo.com>
2021-06-24 22:33:08 +0300

    prints 10 times the numbers, from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * print_most_numbers - prints 10 times the numbers, from 0 to 14
+ * more_numbers - prints 10 times the numbers, from 0 to 14
 */
 void more_numbers(void)
 {
