88909ca2295916513a8d21ce2287305aaf395b27
johnmerga <johnmerga@yahoo.com>
2021-06-24 14:22:12 +0300

    Holberton header file

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -5,5 +5,7 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(month, day, year);
 
 #endif /* HOLBERTON_H */
