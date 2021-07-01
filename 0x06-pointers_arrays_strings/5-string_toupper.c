518180fc0448a03e15bf85701b90f096a0cd7c5f
johnmerga <johnmerga@yahoo.com>
2021-07-01 22:10:21 +0300

    changes all lowercase letters of a string

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,21 +1,21 @@
 #include "holberton.h"
-
 /**
- * string_toupper - changes all lowercase letters of a string to uppercase.
- * @s: - input pointer.
- * Return: s.
-*/
+ * string_toupper - changes all lowercase letters of a string
+ * to uppercase
+ * @s: input string.
+ * Return: the pointer to dest.
+ */
+
 char *string_toupper(char *s)
 {
-	int counter = 0, converter;
+	int count = 0;
 
-	while (*(s + counter) != '\0')
+	while (*(s + count) != '\0')
 	{
-		if ((s[counter] >= 97) && (s[counter] <= 122))
-		{
-			s[counter] = 90 - (122 - s[counter]);
-		}
-		counter++;
+		if ((*(s + count) >= 97) && (*(s + count) <= 122))
+			*(s + count) = *(s + count) - 32;
+		count++;
 	}
+
 	return (s);
 }
