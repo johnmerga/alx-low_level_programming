c1422a7299968a1c29a9e61bdb37d8573b64cc16
johnmerga <johnmerga@yahoo.com>
2021-07-01 22:09:24 +0300

    concatenates two strings,

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,45 +1,27 @@
 #include "holberton.h"
-
 /**
- * _strncat -  a function that concatenates two strings.
- * @dest: - input pointer.
- * @src: - input pointer.
- * @n: - input integer.
- * Return: dest.
-*/
-
+ * _strncat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes used from src.
+ * Return: the pointer to dest.
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-	int counter = 0, counter2 = 0;
+	int count = 0, count2 = 0;
 
-	while (*(dest + counter) != '\0')
+	while (*(dest + count) != '\0')
 	{
-		counter++;
-	}
-	if (n > 0)
-	{
-		while (counter2 >= 0)
-		{
-			if (counter2 == n)
-				break;
-			*(dest + counter) = *(src + counter2);
-			if ((*(src + counter2) == '\0') && (n > counter2))
-				break;
-			counter2++;
-			counter++;
-		}
-	}
-	else
-	{
-		while (counter2 >= 0)
-		{
-			*(dest + counter) = *(src + counter2);
-			if (*(src + counter2) == '\0')
-				break;
-			counter2++;
-			counter++;
-		}
+		count++;
 	}
 
+	while (count2 < n)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
 	return (dest);
 }
