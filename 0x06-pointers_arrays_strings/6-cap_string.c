e035d42ac7ac5b6233d20e5a362859dda5ef34d2
johnmerga <johnmerga@yahoo.com>
2021-07-01 22:10:46 +0300

    capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,29 +1,30 @@
 #include "holberton.h"
-
 /**
- * cap_string -  a function that capitalizes all words of a string.
- * @s: - input pointer.
- * Return: s.
-*/
+ * cap_string - capitalizes all words of a string
+ * @s: input string.
+ * Return: the pointer to dest.
+ */
 
 char *cap_string(char *s)
 {
-	int separeter[] = {9, 32, 92, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
-	int counter = 0, i;
+	int count = 0, i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	for (; counter >= 0 && (*(s + counter) != '\0'); counter++)
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
 		for (i = 0; i < 13; i++)
 		{
-			if (s[counter] == separeter[i])
+			if (*(s + count) == sep_words[i])
 			{
-				if ((s[counter + 1] >= 97) && (s[counter + 1] <= 122))
-				{
-					s[counter + 1] = 90 - (122 - s[counter + 1]);
-				}
-				i++;
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
 			}
 		}
+		count++;
 	}
 	return (s);
 }
