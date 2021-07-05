b0950171afb75fb4f96a2286257b7283322eeefe
johnmerga <johnmerga@yahoo.com>
2021-07-05 20:10:06 +0300

    set string function

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,10 +1,12 @@
+
 #include "holberton.h"
 
 /**
  * set_string - sets the value of a pointer to a char
- * @s: source adress.
- * @to: target adress.
- * Return: no return.
+ * @s: pointer to change
+ * @to: string to change pointer to
+ *
+ * Return: void
  */
 void set_string(char **s, char *to)
 {
